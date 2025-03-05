0501aeca83ee3ca5bce70a2e793a4f04a7e2ef67
Stephanie VerSteeg <shigginsdev@gmail.com>
2025-03-05 16:42:36 -0700

    added auth import

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { v4 as uuidv4 } from 'uuid';
 import { Authenticator } from '@aws-amplify/ui-react';
 import { Amplify } from 'aws-amplify';
+import { Auth } from 'aws-amplify';
 import awsExports from './aws-exports';
 import '@aws-amplify/ui-react/styles.css';
 
