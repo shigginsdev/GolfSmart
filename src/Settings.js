d22af171318d59a81b56e0b9c081e83203498628
Stephanie VerSteeg <shigginsdev@gmail.com>
2025-03-11 18:15:04 -0600

    adding user email for new accounts

--- src/Settings.js
+++ src/Settings.js
@@ -47,7 +47,8 @@ const Settings = ({ user }) => {
           setFormData({
             firstName: userData.data.firstName || '',
             lastName: userData.data.lastName || '',
-            email: userData.data.email || user?.attributes?.email || '',            
+            //email: userData.data.email || user?.attributes?.email || '',            
+            email: userData.data?.email || user?.attributes?.email || '',
             homeCourse: userData.data.homeCourse || '',
             scoringType: userData.data.scoringType || 'Normal Scoring',
             teeBox: userData.data.teeBox || 'Championship Back',
