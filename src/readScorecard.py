6f19823c205497ef10a4bf031a45bb26397300c9
Stephanie VerSteeg <shigginsdev@gmail.com>
2025-03-13 13:00:26 -0600

    openai integration

--- src/readScorecard.py
+++ src/readScorecard.py
@@ -1,22 +1,22 @@
-from openai import OpenAI
+# from openai import OpenAI
 
-client = OpenAI(
-  api_key=secrets
+# client = OpenAI(
+#   api_key=""
 
-response = client.chat.completions.create(
-    model="gpt-4o-mini",
-    messages=[{
-        "role": "user",
-        "content": [
-            {"type": "text", "text": "Provide a list of all scores, holes 1 - 18, for Connor from the attached scorecard. Provide the output as json key value pairs with the hold number and Bobby's score for that hole. If you are unsure on a particular hole, respond with Unk."},
-            {
-                "type": "image_url",
-                "image_url": {
-                    "url": "https://live.staticflickr.com/65535/54384461051_d070850925.jpg",
-                },
-            },
-        ],
-    }],
-)
+# response = client.chat.completions.create(
+#     model="gpt-4o-mini",
+#     messages=[{
+#         "role": "user",
+#         "content": [
+#             {"type": "text", "text": "Provide a list of all scores, holes 1 - 18, for Connor from the attached scorecard. Provide the output as json key value pairs with the hold number and Bobby's score for that hole. If you are unsure on a particular hole, respond with Unk."},
+#             {
+#                 "type": "image_url",
+#                 "image_url": {
+#                     "url": "https://live.staticflickr.com/65535/54384461051_d070850925.jpg",
+#                 },
+#             },
+#         ],
+#     }],
+# )
 
-print(response.choices[0].message.content)
+# print(response.choices[0].message.content)
