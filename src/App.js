dd1ab5b1c5f352041cb60c1e7c4b128cca153436
Stephanie VerSteeg <shigginsdev@gmail.com>
2025-06-26 13:22:54 -0600

    adding javascript error monitoring

--- src/App.js
+++ src/App.js
@@ -11,6 +11,31 @@ import Settings from "./Settings";
 import Coaching from "./coaching";
 import Pricing from "./pricing";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { AwsRum } from 'aws-rum-web';
+
+try {
+  const config = {
+    sessionSampleRate: 1 ,
+    endpoint: "https://dataplane.rum.us-east-2.amazonaws.com" ,
+    telemetries: ["performance","errors","http"] ,
+    allowCookies: true ,
+    enableXRay: false ,
+    signing: false // If you have a public resource policy and wish to send unsigned requests please set this to false
+  };
+
+  const APPLICATION_ID = '491787bd-e832-45b0-a85d-23d55fa84b26';
+  const APPLICATION_VERSION = '1.0.0';
+  const APPLICATION_REGION = 'us-east-2';
+
+  const awsRum = new AwsRum(
+    APPLICATION_ID,
+    APPLICATION_VERSION,
+    APPLICATION_REGION,
+    config
+  );
+} catch (error) {
+  // Ignore errors thrown during CloudWatch RUM web client initialization
+}
 
 Amplify.configure(awsExports);
 
