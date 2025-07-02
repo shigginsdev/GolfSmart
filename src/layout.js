a94e67f14d230cb8c9daf70e59c02c8a55441f50
Stephanie VerSteeg <shigginsdev@gmail.com>
2025-07-02 16:25:22 -0600

    disable menu when new user has not completed their user profile

--- src/layout.js
+++ src/layout.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import './Layout.css';
+import './layout.css';
 
 export default function Layout({ disableNav, signOut, user, children }) {
   return (
