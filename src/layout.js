e4639c31a9c0222203e0c386a0caeadd60af7ae4
Stephanie VerSteeg <shigginsdev@gmail.com>
2025-07-02 16:48:34 -0600

    disable menu when new user has not completed their user profile

--- src/layout.js
+++ src/layout.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, useLocation, NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import './layout.css';
 import bannerImg from './assets/img/edwin-compton-Z8XlmAj65iM-unsplash.png';
 
